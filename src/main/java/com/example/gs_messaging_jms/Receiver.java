da4efcaecf5513b75f1800bdaf11333542b2712f
panpanYuya <yuya@sanbakaraisu.com>
2024-08-29 19:56:59 +0900

    study

---
+++ src/main/java/com/example/gs_messaging_jms/Receiver.java
@@ -0,0 +1,13 @@
+package com.example.gs_messaging_jms;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+    @JmsListener(destination = "mailbox", containerFactory = "myFactory")
+    public void receiveMessage(Email email) {
+        System.out.println("Received <" + email + ">");
+    }
+}
---
+++ src/test/java/com/example/gs_messaging_jms/GsMessagingJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gs_messaging_jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsMessagingJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
